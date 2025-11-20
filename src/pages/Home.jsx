a244d71a1c26caaf6356232032fa7117c56a9a68
Joseph Pal <josephpal27@gmail.com>
2025-11-20 16:05:54 +0530

    Fonts updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,15 @@
 import '../css/Home.css';
+import homeBanner from '/images/banners/home-banner.png';
 
 const Home = () => {
   return (
     <>
-      
+      <section className="banner">
+        <img src={homeBanner} alt="Banner" loading="eager" />
+        <div className="banner-content">
+            
+        </div>
+      </section>
     </>
   )
 }
