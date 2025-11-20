de090981baf98bbc47b9c46c59666a4131fe2678
Joseph Pal <josephpal27@gmail.com>
2025-11-20 16:37:14 +0530

    Home Banner added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,16 +2,19 @@ import '../css/Home.css';
 import homeBanner from '/images/banners/home-banner.png';
 
 const Home = () => {
-  return (
-    <>
-      <section className="banner">
-        <img src={homeBanner} alt="Banner" loading="eager" />
-        <div className="banner-content">
-            
-        </div>
-      </section>
-    </>
-  )
+    return (
+        <>
+            {/* Banner */}
+            <section className="banner">
+                <img src={homeBanner} alt="Banner" loading="eager" />
+                <div className="banner-content">
+                    <h1>Lorem ipsum dolor sit amet, <br /> consectetur adipiscing elit, sed do</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus, nesciunt excepturi. Dicta quo et provident eos laborum debitis asperiores voluptatem qui, eaque possimus iste sit? Et inventore provident facilis sit!</p>
+                    <button>Talk To Our Experts</button>
+                </div>
+            </section>
+        </>
+    )
 }
 
 export default Home
