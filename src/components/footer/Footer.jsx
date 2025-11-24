168a88e4df9d562e0639c59c618f2bfd5fe8998b
Joseph Pal <josephpal27@gmail.com>
2025-11-24 11:47:49 +0530

    Footer added

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,13 +1,29 @@
 import './Footer.css';
+import logo from "../../assets/images/logo/logo-white.png";
+import { FaPhone, FaLocationDot } from "react-icons/fa6";
+import { IoIosMailOpen } from "react-icons/io";
 
 const Footer = () => {
-  return (
-    <>
-      <footer>
-        
-      </footer>
-    </>
-  )
+    return (
+        <>
+            <footer>
+                <div className="foot-content">
+                    <div className="foot-left">
+                        <img src={logo} alt="Logo" loading="lazy" />
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta facilis, at reiciendis labore explicabo impedit dolorum architecto modi facere dolor?</p>
+                    </div>
+                    <div className="foot-right">
+                        <a href="#"> <FaLocationDot className="foot-icon" /> WeWork, DLF Two Horizon Centre, Golf Course Road, Sector 43, Gurugram, Haryana 122002</a>
+                        <a href="#"> <FaPhone className="foot-icon" /> +91 9654408464</a>
+                        <a href="#"> <IoIosMailOpen className="foot-icon" /> pr@confiancecommunications.com</a>
+                    </div>
+                </div>
+                <div className="copyright">
+                    <p>Copyright © 2025 | Confiance Communications | All Rights Reserved</p>
+                </div>
+            </footer>
+        </>
+    )
 }
 
 export default Footer
