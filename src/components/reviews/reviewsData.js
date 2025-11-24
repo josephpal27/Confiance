bd83c7dee2ccda53b77816a6412e656f9815d71d
Joseph Pal <josephpal27@gmail.com>
2025-11-24 12:16:36 +0530

    Client Reviews content updated

--- src/components/reviews/reviewsData.js
+++ src/components/reviews/reviewsData.js
@@ -1,6 +1,6 @@
 export const reviewsData = [
   {
-    image: "/images/reviews/1.webp",
+    image: "/images/reviews/1.avif",
     name: "H. Deniz Ersoz",
     designation:
       "Culture & Tourism Counsellor, Embassy of the Republic of Turkiye",
@@ -8,154 +8,154 @@ export const reviewsData = [
       "Choosing to collaborate with Confiance was driven by Bushra’s profound insight into the sentiments of Indian consumers regarding international travel. I take great satisfaction in recognizing them as an exemplary partner for the India campaign of Türkiye Tourism. Throughout our association, Confiance has consistently provided valuable strategic inputs and leveraged relationships to establish robust awareness around Türkiye’s locations as premier travel, leisure, and wedding destinations. I anticipate many more years of a fruitful partnership with Confiance, relying on their expertise as our strategic communication advisor for India.",
   },
   {
-    image: "/images/reviews/2.webp",
+    image: "/images/reviews/2.avif",
     name: "Eshwar K Vikas",
     designation: "Co-founder, Beyond Appliances",
     review:
       "It’s rare to find a PR team that feels like an extension of your own organization. Confiance has been hands-on, responsive, and aligned with our vision from Day 1. We’ve seen real momentum in our media coverage, thanks to their clear understanding of our brand story and their role in helping us gain meaningful visibility.",
   },
   {
-    image: "/images/reviews/3.webp",
+    image: "/images/reviews/3.avif",
     name: "Mohit Uppal",
     designation: "Head of Marketing & Partnerships, Hoi",
     review:
       "Confiance has played a great role in helping us sharpen Hoi’s brand voice in a competitive travel-tech environment. Bushra and her team quickly understood the core of our platform and the communication challenges that come with building awareness around smart airport solutions. Their ability to distill complex product features into simple, effective messaging has helped us connect better with both travellers and industry stakeholders. Their structured approach, consistency, and understanding of the startup landscape have made them a reliable partner as we scale up.",
   },
   {
-    image: "/images/reviews/4.webp",
+    image: "/images/reviews/4.avif",
     name: "Ravi Srivastava",
     designation: "Partner, Leo Capital",
     review:
       "It’s been a pleasure working with Bushra and the entire Confiance team. They have been meticulous about getting the right messaging out for our portfolio companies at the appropriate times, and we much appreciate the outreach.",
   },
   {
-    image: "/images/reviews/5.webp",
+    image: "/images/reviews/5.avif",
     name: "Malvika Khajuria",
     designation: "COO, LadiesWhoLead",
     review:
       "I’ve had the privilege of collaborating with Bushra/Confiance, on 2-3 different projects so far. Bushra and her team consistently infuse each project with forward focused energy and genuine care, ensuring that specific objectives set are met. What distinguishes Confiance is their pragmatic approach and unwavering commitment to delivering on their promises. With Bushra at the helm, you can have complete confidence that they will go the extra mile to guarantee your success.",
   },
   {
-    image: "/images/reviews/6.webp",
+    image: "/images/reviews/6.avif",
     name: "Raiyaan Shingati",
     designation: "Founder & Managing Partner, Transition VC",
     review:
       "We have had the pleasure of working with Confiance Communications as our PR agency for Transition VC since April 2023, and it has been an outstanding partnership thus far. Since our fund’s inception, Confiance has proven to be a valuable collaborator. As India’s first energy transition-focused VC fund and as thought leaders in the space, we believe that Confiance has played a significant role in representing Transition VC and our vision effectively. Their efforts have not only enhanced our image but also aligned seamlessly with our goals and vision for a sustainable tomorrow. We appreciate the dedication and expertise that Confiance Communications brings to our communications strategy.",
   },
   {
-    image: "/images/reviews/7.webp",
+    image: "/images/reviews/7.avif",
     name: "Maneesh Dhooper",
     designation: "Co-founder, PlanetSpark",
     review:
       "Confiance has been a great partner for our strategic PR. From day 1, the team had a very structured plan and it was executed very efficiently. We got a lot of intelligent insights into how to position ourselves and how to turn our updates into PR-worthy articles. The team consistently over-delivered on its targets. Bushra is an extremely energetic founder and has been involved throughout the journey. This was very important for us as the person who pitched to us and who oversaw operations was the same. The team has a great network and bond with the journalists and is quite perseverant. Overall, it was a great experience working with Confiance and I have recommended the company to a lot of my friends. I just wish they had a referral bonus.",
   },
   {
-    image: "/images/reviews/8.webp",
+    image: "/images/reviews/8.avif",
     name: "Mohit Patel",
     designation: "Founder, The Northstar School | Raga Svara",
     review:
       "I consider Bushra and Team Confiance as our collaborators. They understand and listen to what partners/clients are looking for. I can always trust them to do the right thing for our organisation. I know that everyday, their team is working to ensure that the work that my team does is presented in the right spirit and to the right media. Bushra’s deep understanding of the media landscape and the persistent efforts put in by the whole team always results in positive outcomes. I am certain they have many decades of success ahead of them.",
   },
   {
-    image: "/images/reviews/9.webp",
+    image: "/images/reviews/9.avif",
     name: "Sanjna Yadav",
     designation: "Head of Marketing, The Quorum & district150",
     review:
       "As the Head of Marketing of The Quorum & district150, I am grateful for Confiance’s invaluable partnership over the years. For unique brands like ours, PR strategies need an elevated approach and Confiance has managed to bring interesting ideas to the table consistently. From the inception of our partnership to our current nationwide expansion, their PR expertise have played a pivotal role in our company’s visibility. We’ve not only increased our reach but also garnered the right attention, attracting a diverse & engaged clientele. As Confiance embarks on a new phase of growth, I want to express my sincere gratitude to their entire team. Their efficiency, professionalism, and genuine passion for what they do have made all the difference. I look forward to many more years of this successful collaboration as we continue to expand and achieve even greater heights.",
   },
   {
-    image: "/images/reviews/10.webp",
+    image: "/images/reviews/10.avif",
     name: "Saswat Sundar",
     designation: "Co-founder, Fundamental VC",
     review:
       "I’ve had the opportunity to collaborate with Confiance for the launch of our maiden fund and various deal announcements subsequently. Their team’s unwavering dedication to quality consistently reflected in their output, effective strategies, and meticulous project management. During our fund’s launch, their adeptness in comprehending our desired market positioning, and weaving a strong narrative around it, displayed their exceptional storytelling prowess. Their approach significantly amplified our visibility among stakeholders, proving invaluable to our objectives. I recommend Confiance to all those seeking PR partners with a skin-in-the-game approach. Their professionalism and track record for delivering beyond expectations speak volumes about their service.",
   },
   {
-    image: "/images/reviews/11.webp",
+    image: "/images/reviews/11.avif",
     name: "Joy Sharma",
     designation: "Founder & CEO, ArabEasy",
     review:
       "During my collaboration with Bushra and the Confiance team, their professionalism and commitment to delivering the best possible results were noteworthy. Their tailored approach to our Gulf region-specific PR requirements helped set context for the business for the larger audience in these markets. I have been and will continue to strongly recommend Confiance for their strategic acumen, promptness, work ethic and ability to deliver the promised results.",
   },
   {
-    image: "/images/reviews/12.webp",
+    image: "/images/reviews/12.avif",
     name: "Piyush Nangru",
     designation: "Co-founder & COO, Sunstone",
     review:
       "As Co-founder & COO at Sunstone, our partnership with Bushra and the Confiance team has been truly delightful. Their pivotal role in Sunstone’s brand development during its early stages, especially in spearheading our national and regional communications strategy, significantly contributed to our brand’s growing nationwide presence. Their expertise and strategic insights were evident in their campaign suggestions, which played a key role in efficiently amplifying our vision among the right audience. I have personally recommended Confiance to numerous businesses and startups based on my positive experiences, and they have proved their commitment to quality consistently.",
   },
   {
-    image: "/images/reviews/13.webp",
+    image: "/images/reviews/13.avif",
     name: "Yatin Hans",
     designation: "Co-Founder, Bigsmall.in",
     review:
       "At Bigsmall.in , we collaborated with Confiance for the past 4-5 months. Public relations are extremely important for any brand to truly blossom – any great idea needs execution, and a major part of the execution is to convey the right messages to the clientele. The right PR person helps doing just that – showcasing the brand in the right way, and Conficnace did not only just that – but much more. They truly understood the ethos of our brand, what our philosophy was and what, as a brand, we wanted to stand out for, and convey to the audience. They were always asking the right questions and bringing in fresh and creative ideas from their end. Bushra was also always ready to have brainstorming sessions to aid us, and bring out better execution from our ideas, and to contribute her own. She was extremely dedicated to highlighting the brand’s presence, just the way we had envisioned it to be. It was certainly a great pleasure working with them.",
   },
   {
-    image: "/images/reviews/14.webp",
+    image: "/images/reviews/14.avif",
     name: "Rashi Narang",
     designation: "Founder & CEO, Heads Up For Tails",
     review:
       "Confiance is a result-oriented organization that really knows their work. Bushra is go-getter, a doer, a problem solver and is a great asset for an individual or company who needs professional expertise in the digital and communication space!",
   },
   {
-    image: "/images/reviews/15.webp",
+    image: "/images/reviews/15.avif",
     name: "Nidhi Agarwal",
     designation: "Former Founder, KAARYAH",
     review:
       "Confiance is focused and sharp like an arrow organization. They have the knack of prioritizing the most effective tactics from others and go after it full force. There is no barrier they cannot overcome and there is no effort they are unwilling to attempt. High ownership and very adaptable. An excellent commercial negotiator. Bushra’s EQ and intuition kick in effectively to cover that to her commercial advantage in rate negations and barters. She made some unbelievable deals to KAARYAH and some moon shot alliances possible which were a win-win, ergo sustained for years.",
   },
   {
-    image: "/images/reviews/16.webp",
+    image: "/images/reviews/16.avif",
     name: "Sudeep Singh",
     designation: "CEO & Chief Evangelist, GoWork",
     review:
       "Confiance communications stepped in as not just as our communication partners, but also strategic partners. They have gone over and above mainstream PR, to help build our brand and image. From conceiving social media campaigns to additional value-added features for our community members, they have stopped at nothing to create measurable impact.",
   },
   {
-    image: "/images/reviews/17.webp",
+    image: "/images/reviews/17.avif",
     name: "Swati Bhargava",
     designation: "Co-Founder, CashKaro.com",
     review:
       "We had onboarded Confiance as our PR agency a year ago. They have been a great value addition to our team. With strong ideas and communication, they have helped us enhance our PR strategy. The best part of working with Confiance is that they are always up for brainstorming and stick to deadlines.",
   },
   {
-    image: "/images/reviews/18.webp",
+    image: "/images/reviews/18.avif",
     name: "Bhavna Juneja",
     designation: "Co-Founder & CSO, MPowered",
     review:
       "The first time I met Bushra, who is the owner of Confiance communications I was very impressed looking at a young lady walking in with a man’s confidence and spoke her subject so well. Particularly the work that they have done for our company being that I am from the USA, they did a great job in bringing my story to the media and publications here in terms of the brand visibility and the journey I have carried and why I decided to come into the real estate industry. Their team members are fantastic and she is a great symbol of hiring people with great talent and I see a lot of women also in her company. I wish them the best and I feel they are a bang on company if you want your PR activities and brand engagement done!",
   },
   {
-    image: "/images/reviews/19.webp",
+    image: "/images/reviews/19.avif",
     name: "Manu Jain",
     designation: "Co-Founder, VAMA.app",
     review:
       "Over the past few years, Confiance Communications has played a crucial role in establishing VAMA.app’s digital presence and voice in the rapidly growing astrotech industry. When we started out, our sector was relatively less talked about. Confiance has been instrumental in evoking active discussions about our industry and helping VAMA claim its thought leadership in these conversations. Bushra and her team feel like a natural extension of our own, given their close involvement in VAMA as a growing faith tech platform.",
   },
   {
-    image: "/images/reviews/20.webp",
+    image: "/images/reviews/20.avif",
     name: "Shreyans Jain",
     designation: "Founder, Nutrabay",
     review:
       "Over the past few months, Confiance has been an extension of our team and has supported us in executing organizational narratives through key media. The partnership has grown from strength to strength and we hope that it continues in the future as well.",
   },
   {
-    image: "/images/reviews/21.webp",
+    image: "/images/reviews/21.avif",
     name: "Mohit Bhandari",
     designation: "Co-Founder, Stratzy",
     review:
       "Working with Confiance has been a great experience, the team is good with timely updates on the tasks picked and impact driven planning. All in all a great partner to have for getting your story out there!",
   },
   {
-    image: "/images/reviews/22.webp",
+    image: "/images/reviews/22.avif",
     name: "Rishabh P Nair",
     designation: "Head Of Brand, Content & PR, TIL Limited",
     review:
       "In a short span of time Confiance Communications has proven to be a steadfast partner in articulating TIL Limited’s manufacturing excellence and innovation story. Their team brings a unique blend of strategic thinking and sectoral understanding vis-à-vis news amplification. What stands out is their promptness and keenness to absorb and turn complex technical achievements into compelling narratives. They also understand how important it is to imbibe TIL Limited’s core values to achieve our ultimate vision. We are excited to see how Confiance helps our story resonate and stand our in the world of engineering solutions and manufacturing.",
   },
   {
-    image: "/images/reviews/23.webp",
+    image: "/images/reviews/23.avif",
     name: "Anish Popli",
     designation: "Founder, ProcMart",
     review:
