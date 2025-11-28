0ae8c5dd8b9655b6f99015e67923af6cc2c0c05a
Joseph Pal <josephpal27@gmail.com>
2025-11-28 16:07:34 +0530

    Error Page added

---
+++ src/pages/Error.jsx
@@ -0,0 +1,13 @@
+import '../css/Error.css';
+
+const Error = () => {
+    return (
+        <>
+            <div className="error">
+                <h1 data-aos="fade">PAGE NOT FOUND</h1>
+            </div>
+        </>
+    )
+}
+
+export default Error
