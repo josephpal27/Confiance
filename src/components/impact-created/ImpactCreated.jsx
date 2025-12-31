a27a6441ee1eeba11eeb9e96c204892aaf3a65e6
Joseph Pal <josephpal27@gmail.com>
2025-12-31 17:18:46 +0530

    Microphone elements removed

--- src/components/impact-created/ImpactCreated.jsx
+++ src/components/impact-created/ImpactCreated.jsx
@@ -1,6 +1,6 @@
 import './ImpactCreated.css';
 import impactImage from '../../assets/images/impact-created.avif';
-import micImage from '../../assets/images/announce.avif';
+// import micImage from '../../assets/images/announce.avif';
 
 const ImpactCreated = () => {
   return (
@@ -15,7 +15,7 @@ const ImpactCreated = () => {
                 <img src={impactImage} alt="Impact Created" loading="lazy" />
             </div>
         </div>
-        <img src={micImage} alt="Annouce" loading="lazy" id="announce"/>
+        {/* <img src={micImage} alt="Annouce" loading="lazy" id="announce"/> */}
       </section>
     </>
   )
