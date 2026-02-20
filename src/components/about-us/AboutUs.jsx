03740729de4796ec263fed6a5f24547bfa75a245
Joseph Pal <josephpal27@gmail.com>
2026-02-20 17:52:59 +0530

    About Us Updated

--- src/components/about-us/AboutUs.jsx
+++ src/components/about-us/AboutUs.jsx
@@ -1,46 +1,13 @@
 import './AboutUs.css';
 import aboutImage from '../../assets/images/about-us.avif';
-import { FaStar } from "react-icons/fa6";
 import { RiArrowDownWideFill } from "react-icons/ri";
 
 const AboutUs = () => {
   return (
     <>
       <section className="about-us">
-        <div className="about-us-line-container">
-            <div className="about-line">
-                <p>Confiance <FaStar className="star-icon" /></p>
-                <p>Confiance <FaStar className="star-icon" /></p>
-                <p>Confiance <FaStar className="star-icon" /></p>
-                <p>Confiance <FaStar className="star-icon" /></p>
-                <p>Confiance <FaStar className="star-icon" /></p>
-                <p>Confiance <FaStar className="star-icon" /></p>
-                <p>Confiance <FaStar className="star-icon" /></p>
-                <p>Confiance <FaStar className="star-icon" /></p>
-            </div>
-            <div className="about-line">
-                <p>Confiance <FaStar className="star-icon" /></p>
-                <p>Confiance <FaStar className="star-icon" /></p>
-                <p>Confiance <FaStar className="star-icon" /></p>
-                <p>Confiance <FaStar className="star-icon" /></p>
-                <p>Confiance <FaStar className="star-icon" /></p>
-                <p>Confiance <FaStar className="star-icon" /></p>
-                <p>Confiance <FaStar className="star-icon" /></p>
-                <p>Confiance <FaStar className="star-icon" /></p>
-            </div>
-            <div className="about-line">
-                <p>Confiance <FaStar className="star-icon" /></p>
-                <p>Confiance <FaStar className="star-icon" /></p>
-                <p>Confiance <FaStar className="star-icon" /></p>
-                <p>Confiance <FaStar className="star-icon" /></p>
-                <p>Confiance <FaStar className="star-icon" /></p>
-                <p>Confiance <FaStar className="star-icon" /></p>
-                <p>Confiance <FaStar className="star-icon" /></p>
-                <p>Confiance <FaStar className="star-icon" /></p>
-            </div>
-            <div className="about-rounded-image">
-                <img src={aboutImage} alt="About Us" loading="lazy" data-aos="fade" data-aos-once="true" />
-            </div>
+        <div className="about-us-image">
+          <img src={aboutImage} alt="Founder" loading="lazy" />
         </div>
         <div className="about-us-content">
             <h2 data-aos="fade-up">About Us</h2>
