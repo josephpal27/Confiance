138ded314c78f646d350f9019be5187efd659ee0
Joseph Pal <josephpal27@gmail.com>
2026-02-21 15:42:54 +0530

    About Us Updated

--- src/components/about-banner/AboutBanner.jsx
+++ src/components/about-banner/AboutBanner.jsx
@@ -6,7 +6,7 @@ const AboutBanner = () => {
             <section className="about-banner">
                 <img src="/images/banners/home-banner.avif" alt="Banner" loading="eager" />
                 <div className="banner-content">
-                    <h1 data-aos="fade-up">About Us</h1>
+                    <h1 data-aos="fade-up">Know Your PR Partner</h1>
                     <p data-aos="fade">Founded by seasoned Publicist Bushra Ismail, Confiance emerged from a clear industry gap: public relations had become overly focused on promotion and short-term optics, while critical leadership thinking, business risk, and strategic inflection points remained absent from public discourse.</p>
                     <p data-aos="fade">Confiance was shaped to reset this approach — by working closely with founders and leadership teams, extracting substance from boardroom conversations, and converting it into disciplined, credible public narratives. Today, it functions as a strategic Communications studio that operates as a long-term reputation partner, embedding into decision-making processes and shaping market perception with judgement, restraint, and institutional rigour.</p>
                 </div>
