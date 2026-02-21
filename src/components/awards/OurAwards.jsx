2261ddeff9a053e8da8c8aa686e17f88353b7bd2
Joseph Pal <josephpal27@gmail.com>
2026-02-21 13:08:40 +0530

    Awards Updated

--- src/components/awards/OurAwards.jsx
+++ src/components/awards/OurAwards.jsx
@@ -2,113 +2,101 @@ import './OurAwards.css';
 import { Tab, Nav } from "react-bootstrap";
 import { FaArrowRight } from "react-icons/fa6";
 
+const awardsData = [
+    {
+        key: "tabOne",
+        title: "Afaqs CommuniCon",
+        img: "/images/awards/1.avif",
+        link: "https://events.afaqs.com/communicon-2025/Awards/winners"
+    },
+    {
+        key: "tabTwo",
+        title: "She By Agency Reporter",
+        img: "/images/awards/2.avif",
+        link: "https://www.agencyreporter.com/she24/"
+    },
+    {
+        key: "tabThree",
+        title: "Millennial Achievers 2025",
+        img: "/images/awards/3.avif",
+        link: "https://marketingmind.in/millennial-achievers-2025-presenting-the-winners-who-defined-excellence-this-year/"
+    },
+    {
+        key: "tabFour",
+        title: "Agency Reporter 40Under40",
+        img: "/images/awards/4.avif",
+        link: "https://www.agencyreporter.com/40under40/"
+    },
+    {
+        key: "tabFive",
+        title: "Reputation Today 40Under40",
+        img: "/images/awards/5.avif",
+        link: "https://reputationtoday.in/reputation-today-40-under-forty-class-of-2019/"
+    },
+    {
+        key: "tabSix",
+        title: "E4M 40Under40",
+        img: "/images/awards/6.avif",
+        link: "https://www.exchange4media.com/pr-and-corporate-communication-news/exchange4media-unveils-pr-corporate-communications-40-under-40-list-101574.html"
+    },
+    {
+        key: "tabSeven",
+        title: "Impact Super 50",
+        img: "/images/awards/7.avif",
+        link: "https://www.impactonnet.com/spotlight/celebrating-women-leaders-in-pr-corp-comm-7105.html"
+    },
+    {
+        key: "tabEight",
+        title: "E4M CEO of the Year Award",
+        img: "/images/awards/8.avif",
+        link: "https://www.exchange4media.com/pr-and-corporate-communication-news/47-leaders-honoured-at-first-edition-of-e4m-pr-corp-comm-women-achievers-awards-106465.html"
+    }
+];
+
 const OurAwards = () => {
     return (
-        <>
-            <section className="our-awards">
-                <h6 data-aos="fade-right">Our Awards</h6>
-                <div className="awards-tabbing-container">
-                    <Tab.Container defaultActiveKey="tabOne">
-                        {/* Tabs Content */}
-                        <Tab.Content>
-                            <Tab.Pane eventKey="tabOne" className="fade show">
-                                <img src="/images/awards/1.avif" alt="Tab 1" loading="lazy" />
-                            </Tab.Pane>
-                            <Tab.Pane eventKey="tabTwo" className="fade">
-                                <img src="/images/awards/2.avif" alt="Tab 2" loading="lazy" />
-                            </Tab.Pane>
-                            <Tab.Pane eventKey="tabThree" className="fade">
-                                <img src="/images/awards/3.avif" alt="Tab 3" loading="lazy" />
-                            </Tab.Pane>
-                            <Tab.Pane eventKey="tabFour" className="fade">
-                                <img src="/images/awards/4.avif" alt="Tab 4" loading="lazy" />
-                            </Tab.Pane>
-                            <Tab.Pane eventKey="tabFive" className="fade">
-                                <img src="/images/awards/5.avif" alt="Tab 5" loading="lazy" />
-                            </Tab.Pane>
-                            <Tab.Pane eventKey="tabSix" className="fade">
-                                <img src="/images/awards/6.avif" alt="Tab 6" loading="lazy" />
-                            </Tab.Pane>
-                            <Tab.Pane eventKey="tabSeven" className="fade">
-                                <img src="/images/awards/7.avif" alt="Tab 7" loading="lazy" />
-                            </Tab.Pane>
-                            <Tab.Pane eventKey="tabEight" className="fade">
-                                <img src="/images/awards/8.avif" alt="Tab 8" loading="lazy" />
-                            </Tab.Pane>
-                        </Tab.Content>
+        <section className="our-awards">
+            <h6 data-aos="fade-right">Our Awards</h6>
 
-                        {/* Tabs Navigation */}
-                        <Nav variant="tabs" className="tab-head">
-                            <Nav.Item>
-                                <Nav.Link eventKey="tabOne">
-                                    Afaqs CommuniCon 
-                                    <a href="https://events.afaqs.com/communicon-2025/Awards/winners" target="_blank" rel="noopener noreferrer" onClick={(e) => e.stopPropagation()}>
-                                        View More <FaArrowRight />
-                                    </a>
-                                </Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Nav.Link eventKey="tabTwo">
-                                    She By Agency Reporter
-                                    <a href="https://www.agencyreporter.com/she24/" target="_blank" rel="noopener noreferrer" onClick={(e) => e.stopPropagation()}>
-                                        View More <FaArrowRight />
-                                    </a>
-                                </Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Nav.Link eventKey="tabThree">
-                                    Millennial Achievers 2025
-                                    <a href="https://marketingmind.in/millennial-achievers-2025-presenting-the-winners-who-defined-excellence-this-year/" target="_blank" rel="noopener noreferrer" onClick={(e) => e.stopPropagation()}>
-                                        View More <FaArrowRight />
-                                    </a>
-                                </Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Nav.Link eventKey="tabFour">
-                                    Agency Reporter 40Under40
-                                    <a href="https://www.agencyreporter.com/40under40/" target="_blank" rel="noopener noreferrer" onClick={(e) => e.stopPropagation()}>
-                                        View More <FaArrowRight />
-                                    </a>
-                                </Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Nav.Link eventKey="tabFive">
-                                    Reputation Today 40Under40
-                                    <a href="https://reputationtoday.in/reputation-today-40-under-forty-class-of-2019/" target="_blank" rel="noopener noreferrer" onClick={(e) => e.stopPropagation()}>
-                                        View More <FaArrowRight />
-                                    </a>
-                                </Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Nav.Link eventKey="tabSix">
-                                    E4M 40Under40
-                                    <a href="https://www.exchange4media.com/pr-and-corporate-communication-news/exchange4media-unveils-pr-corporate-communications-40-under-40-list-101574.html%20" target="_blank" rel="noopener noreferrer" onClick={(e) => e.stopPropagation()}>
-                                        View More <FaArrowRight />
-                                    </a>
-                                </Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Nav.Link eventKey="tabSeven">
-                                    Impact Super 50
-                                    <a href="https://www.impactonnet.com/spotlight/celebrating-women-leaders-in-pr-corp-comm-7105.html?fbclid=IwAR0VSkIg9p8ZTvY779NB9e69-oPlsgieoBitK8us3oIw3WxbQzONgZpKVVU" target="_blank" rel="noopener noreferrer" onClick={(e) => e.stopPropagation()}>
-                                        View More <FaArrowRight />
-                                    </a>
-                                </Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Nav.Link eventKey="tabEight">
-                                    E4M CEO of the Year Award
-                                    <a href="https://www.exchange4media.com/pr-and-corporate-communication-news/47-leaders-honoured-at-first-edition-of-e4m-pr-corp-comm-women-achievers-awards-106465.html" target="_blank" rel="noopener noreferrer" onClick={(e) => e.stopPropagation()}>
-                                        View More <FaArrowRight />
-                                    </a>
-                                </Nav.Link>
-                            </Nav.Item>
-                        </Nav>
-                    </Tab.Container>
-                </div>
-            </section>
-        </>
-    )
-}
+            <div className="awards-tabbing-container">
+                <Tab.Container defaultActiveKey={awardsData[0].key}>
 
-export default OurAwards
+                    {/* Tab Content */}
+                    <Tab.Content>
+                        {awardsData.map((award) => (
+                            <Tab.Pane key={award.key} eventKey={award.key}>
+                                <img
+                                    src={award.img}
+                                    alt={award.title}
+                                    loading="lazy"
+                                />
+                            </Tab.Pane>
+                        ))}
+                    </Tab.Content>
+
+                    {/* Tab Navigation */}
+                    <Nav variant="tabs" className="tab-head">
+                        {awardsData.map((award) => (
+                            <Nav.Item key={award.key}>
+                                <Nav.Link eventKey={award.key}>
+                                    {award.title}
+                                    <a
+                                        href={award.link}
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                        onClick={(e) => e.stopPropagation()}
+                                    >
+                                        <span>View More</span> <FaArrowRight />
+                                    </a>
+                                </Nav.Link>
+                            </Nav.Item>
+                        ))}
+                    </Nav>
+
+                </Tab.Container>
+            </div>
+        </section>
+    );
+};
+
+export default OurAwards;
