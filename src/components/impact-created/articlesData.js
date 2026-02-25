8c0e0ecdcdb3a055868cd0b6d83186a2c28397eb
Joseph Pal <josephpal27@gmail.com>
2026-02-25 15:19:32 +0530

    Articles Updated

--- src/components/impact-created/articlesData.js
+++ src/components/impact-created/articlesData.js
@@ -1,13 +1,85 @@
 export const leftArticles = [
-  { id: 1, image: "/images/impact-created/1.png", link: "https://example.com/article1" },
-  { id: 2, image: "/images/impact-created/1.png", link: "https://example.com/article2" },
-  { id: 3, image: "/images/impact-created/1.png", link: "https://example.com/article3" },
-  { id: 4, image: "/images/impact-created/1.png", link: "https://example.com/article4" },
+  {
+    id: 1,
+    image: "/images/impact-created/1.png",
+    link: "https://economictimes.indiatimes.com/small-biz/entrepreneurship/members-only-how-a-vivek-narain-built-a-profitable-private-club-business/articleshow/101916585.cms",
+  },
+  {
+    id: 2,
+    image: "/images/impact-created/2.jpg",
+    link: "https://www.youtube.com/watch?v=qkqheeJM8IQ",
+  },
+  {
+    id: 3,
+    image: "/images/impact-created/5.png",
+    link: "https://example.com/article3",
+  },
+  {
+    id: 4,
+    image: "/images/impact-created/6.jpeg",
+    link: "https://example.com/article4",
+  },
+  {
+    id: 4,
+    image: "/images/impact-created/9.png",
+    link: "https://example.com/article4",
+  },
+  {
+    id: 4,
+    image: "/images/impact-created/10.png",
+    link: "https://example.com/article4",
+  },
+  {
+    id: 4,
+    image: "/images/impact-created/13.png",
+    link: "https://example.com/article4",
+  },
+  {
+    id: 4,
+    image: "/images/impact-created/14.png",
+    link: "https://example.com/article4",
+  },
 ];
 
 export const rightArticles = [
-  { id: 5, image: "/images/impact-created/2.png", link: "https://example.com/article5" },
-  { id: 6, image: "/images/impact-created/2.png", link: "https://example.com/article6" },
-  { id: 7, image: "/images/impact-created/2.png", link: "https://example.com/article7" },
-  { id: 8, image: "/images/impact-created/2.png", link: "https://example.com/article8" },
+  {
+    id: 5,
+    image: "/images/impact-created/3.jpg",
+    link: "https://example.com/article5",
+  },
+  {
+    id: 6,
+    image: "/images/impact-created/4.png",
+    link: "https://example.com/article6",
+  },
+  {
+    id: 7,
+    image: "/images/impact-created/7.png",
+    link: "https://example.com/article7",
+  },
+  {
+    id: 8,
+    image: "/images/impact-created/8.png",
+    link: "https://example.com/article8",
+  },
+  {
+    id: 8,
+    image: "/images/impact-created/11.png",
+    link: "https://example.com/article8",
+  },
+  {
+    id: 8,
+    image: "/images/impact-created/12.png",
+    link: "https://example.com/article8",
+  },
+  {
+    id: 8,
+    image: "/images/impact-created/15.png",
+    link: "https://example.com/article8",
+  },
+  {
+    id: 8,
+    image: "/images/impact-created/16.png",
+    link: "https://example.com/article8",
+  },
 ];
