e88a382b72d09274796d82664282be7f72bfcbee
Joseph Pal <josephpal27@gmail.com>
2026-02-25 16:32:12 +0530

    Article Images Compressed to AVIF

--- src/components/impact-created/articlesData.js
+++ src/components/impact-created/articlesData.js
@@ -1,42 +1,42 @@
 export const leftArticles = [
   {
     id: 1,
-    image: "/images/impact-created/1.png",
+    image: "/images/impact-created/1.avif",
     link: "https://economictimes.indiatimes.com/small-biz/entrepreneurship/members-only-how-a-vivek-narain-built-a-profitable-private-club-business/articleshow/101916585.cms",
   },
   {
     id: 2,
-    image: "/images/impact-created/2.jpg",
+    image: "/images/impact-created/2.avif",
     link: "https://www.youtube.com/watch?v=qkqheeJM8IQ",
   },
   {
     id: 3,
-    image: "/images/impact-created/5.png",
+    image: "/images/impact-created/5.avif",
     link: "https://www.livemint.com/companies/news/luxury-company-quorum-club-ventures-into-meetings-and-event-space-business-with-district150-11687252639035.html",
   },
   {
     id: 4,
-    image: "/images/impact-created/6.jpeg",
+    image: "/images/impact-created/6.avif",
     link: "#",
   },
   {
     id: 5,
-    image: "/images/impact-created/9.png",
+    image: "/images/impact-created/9.avif",
     link: "https://economictimes.indiatimes.com/small-biz/sme-sector/cashkaro-clocks-rs-250-cr-in-revenue-for-fy22-eyes-new-verticals/articleshow/92946667.cms?from=mdr",
   },
   {
     id: 6,
-    image: "/images/impact-created/10.png",
+    image: "/images/impact-created/10.avif",
     link: "https://economictimes.indiatimes.com/small-biz/sme-sector/earnkaros-affiliate-marketing-is-upending-the-social-cashback-game-in-bharat/articleshow/103746812.cms",
   },
   {
     id: 7,
-    image: "/images/impact-created/13.png",
+    image: "/images/impact-created/13.avif",
     link: "https://www.moneycontrol.com/news/business/euronics-looking-to-double-turnover-to-rs-400-crore-by-2025-26-md-ceo-viknesh-jain-12755673.html#google_vignette",
   },
   {
     id: 8,
-    image: "/images/impact-created/14.png",
+    image: "/images/impact-created/14.avif",
     link: "https://www.thehindubusinessline.com/companies/euronics-commences-operations-at-new-manufacturing-plant-in-gurugram/article68336677.ece",
   },
 ];
@@ -44,42 +44,42 @@ export const leftArticles = [
 export const rightArticles = [
   {
     id: 1,
-    image: "/images/impact-created/3.jpg",
+    image: "/images/impact-created/3.avif",
     link: "https://www.youtube.com/watch?v=E1BYR5WSGWE",
   },
   {
     id: 2,
-    image: "/images/impact-created/4.png",
+    image: "/images/impact-created/4.avif",
     link: "https://www.hindustantimes.com/lifestyle/brunch/new-generation-clubs-cater-to-young-ambitious-indians-with-work-entertainment-culture-and-networking-opportunities-and-intimate-spaces-101685138956406.html",
   },
   {
     id: 3,
-    image: "/images/impact-created/7.png",
+    image: "/images/impact-created/7.avif",
     link: "https://www.fortuneindia.com/40under40/rohan-bhargava-swati-bhargava?year=2019",
   },
   {
     id: 4,
-    image: "/images/impact-created/8.png",
+    image: "/images/impact-created/8.avif",
     link: "https://www.forbesindia.com/article/take-one-big-story-of-the-day/how-cashkaro-got-its-payback-from-cashbacks-(and-some-cash-burn)/67889/1",
   },
   {
     id: 5,
-    image: "/images/impact-created/11.png",
+    image: "/images/impact-created/11.avif",
     link: "https://entrackr.com/2023/06/salad-days-focuses-on-healthy-eating-as-a-lifestyle/",
   },
   {
     id: 6,
-    image: "/images/impact-created/12.png",
+    image: "/images/impact-created/12.avif",
     link: "https://retail.economictimes.indiatimes.com/news/food-entertainment/food-services/salad-days-invests-rs-1-25-crore-to-open-3-cloud-kitchens-in-bengaluru/100985311",
   },
   {
     id: 7,
-    image: "/images/impact-created/15.png",
+    image: "/images/impact-created/15.avif",
     link: "https://www.thehindubusinessline.com/markets/procmart-targets-fy28-ipo-as-revenue-set-to-hit-1000-cr-in-fy26/article70280687.ece#google_vignette.com/article8",
   },
   {
     id: 8,
-    image: "/images/impact-created/16.png",
+    image: "/images/impact-created/16.avif",
     link: "https://inc42.com/buzz/procmart-elevates-finance-strategy-head-sachin-jain-to-cofounder/",
   },
 ];
