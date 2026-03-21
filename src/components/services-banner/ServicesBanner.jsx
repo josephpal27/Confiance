8ee2f1eb621ffebaa8ddea4be9983c4a5cb32d18
Joseph Pal <josephpal27@gmail.com>
2026-03-21 11:45:47 +0530

    Services Banner Updated

--- src/components/services-banner/ServicesBanner.jsx
+++ src/components/services-banner/ServicesBanner.jsx
@@ -7,6 +7,7 @@ const ServicesBanner = () => {
                 <img src="/images/banners/services-banner.png" alt="Banner" loading="eager" />
                 <div className="banner-content">
                     <h1 data-aos="fade-up">Our Services</h1>
+                    <p data-aos="fade">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum viverra maecenas accumsan lacus vel facilisis.</p>
                 </div>
             </section>
         </>
