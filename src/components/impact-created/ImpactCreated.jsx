db8517f21b413e6e29bb9554e7366977050df91a
Joseph Pal <josephpal27@gmail.com>
2026-03-27 13:14:21 +0530

    Impact Created Updated

--- src/components/impact-created/ImpactCreated.jsx
+++ src/components/impact-created/ImpactCreated.jsx
@@ -3,6 +3,7 @@ import { leftArticles, rightArticles } from "./articlesData";
 import newsImage from "../../assets/images/news.avif";
 import "./ImpactCreated.css";
 import ArticleCard from "./ArticleCard";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa6";
 
 const ImpactCreated = () => {
 
@@ -11,6 +12,9 @@ const ImpactCreated = () => {
 
   const [leftFlip, setLeftFlip] = useState(false);
   const [rightFlip, setRightFlip] = useState(false);
+
+  const [isAnimating, setIsAnimating] = useState(false);
+  const [isPaused, setIsPaused] = useState(false);
 
   const visibleLeft = [
     leftArticles[leftIndex % leftArticles.length],
@@ -22,30 +26,96 @@ const ImpactCreated = () => {
     rightArticles[(rightIndex + 1) % rightArticles.length]
   ];
 
+  // Pause Auto (resume after 10s)
+  const pauseAuto = () => {
+    setIsPaused(true);
+    setTimeout(() => setIsPaused(false), 10000);
+  };
+
+  // Next Btn
+  const handleNext = () => {
+    if (isAnimating) return;
+
+    pauseAuto();
+    setIsAnimating(true);
+
+    // Left
+    setLeftFlip(true);
+    setTimeout(() => {
+      setLeftIndex(prev => (prev + 2) % leftArticles.length);
+      setLeftFlip(false);
+    }, 700);
+
+    // Right
+    setTimeout(() => {
+      setRightFlip(true);
+      setTimeout(() => {
+        setRightIndex(prev => (prev + 2) % rightArticles.length);
+        setRightFlip(false);
+        setIsAnimating(false);
+      }, 700);
+    }, 1500);
+  };
+
+  // Prev Btn
+  const handlePrev = () => {
+    if (isAnimating) return;
+
+    pauseAuto();
+    setIsAnimating(true);
+
+    // Left
+    setLeftFlip(true);
+    setTimeout(() => {
+      setLeftIndex(prev =>
+        (prev - 2 + leftArticles.length) % leftArticles.length
+      );
+      setLeftFlip(false);
+    }, 700);
+
+    // Right
+    setTimeout(() => {
+      setRightFlip(true);
+      setTimeout(() => {
+        setRightIndex(prev =>
+          (prev - 2 + rightArticles.length) % rightArticles.length
+        );
+        setRightFlip(false);
+        setIsAnimating(false);
+      }, 700);
+    }, 1500);
+  };
+
+  // Auto Slide (continuous)
   useEffect(() => {
+    if (isPaused) return;
 
     const interval = setInterval(() => {
+      if (isAnimating) return;
 
-      // Left Animation
+      setIsAnimating(true);
+
+      // Left
       setLeftFlip(true);
       setTimeout(() => {
         setLeftIndex(prev => (prev + 2) % leftArticles.length);
         setLeftFlip(false);
       }, 700);
 
-      // Right Animation
+      // Right
       setTimeout(() => {
         setRightFlip(true);
         setTimeout(() => {
           setRightIndex(prev => (prev + 2) % rightArticles.length);
           setRightFlip(false);
+          setIsAnimating(false);
         }, 700);
       }, 1500);
+
     }, 7000);
 
     return () => clearInterval(interval);
-
-  }, []);
+  }, [isPaused, isAnimating]);
 
   return (
     <section className="impact-created">
@@ -62,7 +132,8 @@ const ImpactCreated = () => {
 
       <div className="impact-images">
         <div className="impact-image-container">
-          {/* Left*/}
+
+          {/* Left */}
           <div className="impact-left">
             <ArticleCard article={visibleLeft[0]} flip={leftFlip} delay={0} />
 
@@ -78,7 +149,15 @@ const ImpactCreated = () => {
             <ArticleCard article={visibleRight[0]} flip={rightFlip} delay={0} />
             <ArticleCard article={visibleRight[1]} flip={rightFlip} delay={0.2} />
           </div>
+
         </div>
+
+        {/* Controls */}
+        <div className="impact-controls">
+          <button onClick={handlePrev} disabled={isAnimating}><FaArrowLeft /></button>
+          <button onClick={handleNext} disabled={isAnimating}><FaArrowRight /></button>
+        </div>
+
       </div>
 
     </section>
