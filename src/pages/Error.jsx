246ad034626a208bf729a23ee60d0d4d908b2564
Joseph Pal <josephpal27@gmail.com>
2026-03-27 12:21:35 +0530

    Error Page Updated

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -5,7 +5,8 @@ const Error = () => {
     return (
         <>
             <div className="error">
-                <h1 data-aos="fade">Under Development</h1>
+                <h1 data-aos="fade">PAGE NOT FOUND</h1>
+                <p data-aos="fade">The page you are looking for does not exist</p>
                 <Link to="/" data-aos="fade">Back to Home</Link>
             </div>
         </>
