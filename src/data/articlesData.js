8278839622ff08e3479443fc3a754ea26e2c3316
Joseph Pal <josephpal27@gmail.com>
2026-05-14 11:40:32 +0530

    Article Images Updated

--- src/data/articlesData.js
+++ src/data/articlesData.js
@@ -1,72 +1,72 @@
 export const leftArticles = [
   {
     id: 1,
-    image: "/images/impact-created/1.png",
+    image: "/images/impact-created/1.avif",
     link: "https://techcrunch.com/2025/10/15/indias-kuku-snags-85m-as-mobile-content-wars-intensify/",
   },
   {
     id: 2,
-    image: "/images/impact-created/2.png",
+    image: "/images/impact-created/2.avif",
     link: "https://www.instagram.com/p/DUnDQUrEhRL/?img_index=1&igsh=cndyMm0xZ3cwbHB1",
   },
   {
     id: 3,
-    image: "/images/impact-created/5.png",
+    image: "/images/impact-created/5.avif",
     link: "https://www.thehindubusinessline.com/economy/agri-business/demand-for-natural-products-has-surged-in-the-past-decade-just-herbs-ceo/article68442593.ece",
   },
   {
     id: 4,
-    image: "/images/impact-created/6.png",
+    image: "/images/impact-created/6.avif",
     link: "https://inc42.com/startups/how-just-herbs-grew-5x-to-hit-inr-100-cr-revenue-mark-in-3-years-post-marico-takeover/",
   },
   {
     id: 5,
-    image: "/images/impact-created/9.png",
+    image: "/images/impact-created/9.avif",
     link: "https://economictimes.indiatimes.com/small-biz/entrepreneurship/binny-bansal-backed-planetspark-plans-to-go-public-in-two-years/articleshow/118204380.cms?from=mdr",
   },
   {
     id: 6,
-    image: "/images/impact-created/10.png",
+    image: "/images/impact-created/10.avif",
     link: "https://inc42.com/features/planetspark-breakeven-profit-ipo-edtech-funding-winter/",
   },
   {
     id: 7,
-    image: "/images/impact-created/13.png",
+    image: "/images/impact-created/13.avif",
     link: "https://www.thehindubusinessline.com/news/india-will-be-among-the-top-10-markets-in-three-years-for-turkey/article67793177.ece",
   },
   {
     id: 8,
-    image: "/images/impact-created/14.png",
+    image: "/images/impact-created/14.avif",
     link: "https://economictimes.indiatimes.com/nri/visit/trkiye-targeting-3-5-lakh-indian-arrivals-in-2025/articleshow/107269574.cms",
   },
   {
     id: 9,
-    image: "/images/impact-created/17.png",
+    image: "/images/impact-created/17.avif",
     link: "https://www.youtube.com/watch?v=E1BYR5WSGWE",
   },
   {
     id: 10,
-    image: "/images/impact-created/18.png",
+    image: "/images/impact-created/18.avif",
     link: "https://www.hindustantimes.com/lifestyle/brunch/new-generation-clubs-cater-to-young-ambitious-indians-with-work-entertainment-culture-and-networking-opportunities-and-intimate-spaces-101685138956406.html",
   },
   {
     id: 11,
-    image: "/images/impact-created/21.png",
+    image: "/images/impact-created/21.avif",
     link: "https://www.forbesindia.com/article/take-one-big-story-of-the-day/how-cashkaro-got-its-payback-from-cashbacks-(and-some-cash-burn)/67889/1",
   },
   {
     id: 12,
-    image: "/images/impact-created/22.jpeg",
+    image: "/images/impact-created/22.avif",
     link: "https://economictimes.indiatimes.com/small-biz/sme-sector/cashkaro-clocks-rs-250-cr-in-revenue-for-fy22-eyes-new-verticals/articleshow/92946667.cms?from=mdr",
   },
   {
     id: 13,
-    image: "/images/impact-created/25.png",
+    image: "/images/impact-created/25.avif",
     link: "https://www.thehindubusinessline.com/companies/euronics-commences-operations-at-new-manufacturing-plant-in-gurugram/article68336677.ece",
   },
   {
     id: 14,
-    image: "/images/impact-created/26.png",
+    image: "/images/impact-created/26.avif",
     link: "https://www.thehindubusinessline.com/markets/procmart-targets-fy28-ipo-as-revenue-set-to-hit-1000-cr-in-fy26/article70280687.ece#google_vignette",
   },
 ];
@@ -74,72 +74,72 @@ export const leftArticles = [
 export const rightArticles = [
   {
     id: 1,
-    image: "/images/impact-created/3.jpeg",
+    image: "/images/impact-created/3.avif",
     link: "https://www.youtube.com/watch?v=ZNzgA__UTm8&t=83s",
   },
   {
     id: 2,
-    image: "/images/impact-created/4.png",
+    image: "/images/impact-created/4.avif",
     link: "https://www.financialexpress.com/business/brandwagon/chai-pe-charcha-with-brandwagon-165/3536952/",
   },
   {
     id: 3,
-    image: "/images/impact-created/7.png",
+    image: "/images/impact-created/7.avif",
     link: "https://economictimes.indiatimes.com/small-biz/sme-sector/how-beyond-appliances-is-crafting-an-ai-powered-future-of-the-indian-kitchen/articleshow/117074947.cms?from=mdr",
   },
   {
     id: 4,
-    image: "/images/impact-created/8.png",
+    image: "/images/impact-created/8.avif",
     link: "https://www.livemint.com/companies/start-ups/smart-kitchen-appliances-smart-chimneys-hobs-ai-powered-cooking-assistant-iot-emabled-products-11736480895074.html",
   },
   {
     id: 5,
-    image: "/images/impact-created/11.png",
+    image: "/images/impact-created/11.avif",
     link: "https://www.businesstoday.in/latest/trends/story/children-to-pitch-biz-ideas-to-sharks-as-planetspark-announces-shark-tank-spin-off-315849-2021-12-16",
   },
   {
     id: 6,
-    image: "/images/impact-created/12.png",
+    image: "/images/impact-created/12.avif",
     link: "https://www.youtube.com/watch?v=EacxetbV_aU",
   },
   {
     id: 7,
-    image: "/images/impact-created/15.png",
+    image: "/images/impact-created/15.avif",
     link: "https://economictimes.indiatimes.com/small-biz/entrepreneurship/members-only-how-a-vivek-narain-built-a-profitable-private-club-business/articleshow/101916585.cms",
   },
   {
     id: 8,
-    image: "/images/impact-created/16.png",
+    image: "/images/impact-created/16.avif",
     link: "https://www.youtube.com/watch?v=qkqheeJM8IQ",
   },
   {
     id: 9,
-    image: "/images/impact-created/19.png",
+    image: "/images/impact-created/19.avif",
     link: "https://www.livemint.com/companies/news/luxury-company-quorum-club-ventures-into-meetings-and-event-space-business-with-district150-11687252639035.html",
   },
   {
     id: 10,
-    image: "/images/impact-created/20.png",
+    image: "/images/impact-created/20.avif",
     link: "https://www.fortuneindia.com/rankings/40under40/2019",
   },
   {
     id: 11,
-    image: "/images/impact-created/23.png",
+    image: "/images/impact-created/23.avif",
     link: "https://entrackr.com/2023/06/salad-days-focuses-on-healthy-eating-as-a-lifestyle/",
   },
   {
     id: 12,
-    image: "/images/impact-created/24.png",
+    image: "/images/impact-created/24.avif",
     link: "https://www.moneycontrol.com/news/business/euronics-looking-to-double-turnover-to-rs-400-crore-by-2025-26-md-ceo-viknesh-jain-12755673.html#google_vignette",
   },
   {
     id: 13,
-    image: "/images/impact-created/27.png",
+    image: "/images/impact-created/27.avif",
     link: "https://inc42.com/buzz/procmart-elevates-finance-strategy-head-sachin-jain-to-cofounder/",
   },
   {
     id: 14,
-    image: "/images/impact-created/28.png",
+    image: "/images/impact-created/28.avif",
     link: "https://www.moneycontrol.com/news/business/companies/procmart-raises-30-million-led-by-nandan-nilekani-and-fundamentum-partnership-12645551.html",
   },
 ];
